9e7d0cae1eb4bc25f431dc4e2321900761191b98
Harshit Gandhi <gandhiharshit716@gmail.com>
2025-05-18 23:09:58 +0530

    cleared unecessary comments

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      // These options are no longer needed in newer Mongoose versions
-      // but keeping them for compatibility
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
